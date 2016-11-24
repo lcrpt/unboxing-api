619f10c411cbb8734740d07a07da2e223d1f7950
leolassence <leolassence@gmail.com>
2016-11-24 17:55:26 +0100

    updated models with validation and hooks

--- server/models/orders.js
+++ server/models/orders.js
@@ -1,6 +1,28 @@
+import Sequelize from 'sequelize';
+
 export default (sequelize, DataTypes) => {
   const Orders = sequelize.define('orders', {
-    delivery_date: DataTypes.DATE
+    delivery_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      },
+    },
+    customer_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+      },
+    },
+    driver_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+      },
+    },
   }, {
     classMethods: {
       associate(models) {
