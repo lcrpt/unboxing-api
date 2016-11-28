bff320108bb40536ac31a8ef0bdd01d65816e817
leolassence <leolassence@gmail.com>
2016-11-28 14:43:33 +0100

    fixed routes organisation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,12 @@
 import usersRoute from './users';
-import addresses from '../controllers/addresses';
 import addressesRoute from './addresses';
-import ordersRoute from './orders';
 import itemsRoute from './items';
+import ordersRoute from './orders';
 
 export default (router) => {
   usersRoute(router);
   addressesRoute(router);
   itemsRoute(router);
-  
-  router.get('/orders', orders.getOrders);
-  router.get('/orders/:id', orders.getOrder);
-  // router.post('/orders', orders.postOrder);
-  // router.put('/orders/:id', orders.updateOrder);
-  // router.delete('/orders/:id', orders.deleteOrder);
-
   ordersRoute(router);
 
   return router;
